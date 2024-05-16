264a270a7f2d2e7e517ec8c9cbbe2965af126135
douglas w m flores <douglas@spectralsolutions.com.br>
2024-05-16 17:19:26 -0300

    discovery with yolov5

--- app.py
+++ app.py
@@ -10,9 +10,42 @@ import csv
 from flasgger import Swagger
 from flask import render_template
 import chardet
+import cv2
+import torch
 
 app = Flask(__name__, static_url_path='/static')
 swagger = Swagger(app)
+
+# Função para carregar o modelo YOLOv5
+def load_yolo():
+    # Carregar modelo YOLOv5
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+    model.eval()
+    return model
+
+
+def detect_objects(model, img):
+    results = model(img)
+
+    for result in results.xyxy[0]:
+        x1, y1, x2, y2, conf, cls = result
+        if int(cls) == 16 and conf > 0.4:  # 16 é a classe de animais
+            cropped_img = img[int(y1):int(y2), int(x1):int(x2)]
+            # Salvar a imagem recortada
+            cv2.imwrite('cropped_temp.jpg', cropped_img)
+            return cropped_img
+        if int(cls) == 15 and conf > 0.4:  # 15 é a classe de animais
+            cropped_img = img[int(y1):int(y2), int(x1):int(x2)]
+            cv2.imwrite('cropped_temp2.jpg', cropped_img)
+            return cropped_img
+
+    return None
+
+
+# Carregar YOLOv5
+print("Carregando modelo yolov5...")
+net = load_yolo()
+print("Carregado modelo yolov5!")
 
 # Carregar o modelo MobileNetV2 pré-treinado
 print("Carregando modelo MobileNetV2...")
@@ -141,6 +174,71 @@ def predict():
         query_features = base_model.predict(np.expand_dims(img, axis=0)).flatten()
         most_similar_pets = find_most_similar_pet(query_features)
         return jsonify({'most_similar_pets': most_similar_pets}), 200
+    
+
+# Rota de predição
+@app.route('/v2/predict', methods=['POST'])
+def predictv2():
+    """
+    Endpoint para prever os pets mais semelhantes com base em uma imagem enviada.
+    ---
+    parameters:
+      - name: file
+        in: formData
+        type: file
+        required: true
+        description: Imagem do pet (multipart/form-data)
+    responses:
+      200:
+        description: OK
+        schema:
+          id: Prediction
+          properties:
+            most_similar_pets:
+              type: array
+              items:
+                type: string
+                description: URL para o pet mais semelhante
+    """
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    if file:
+        # Salvar a imagem temporariamente
+        image_path = 'temp.jpg'
+        file.save(image_path)
+
+        # Carregar imagem
+        image = cv2.imread(image_path)
+
+        # Detectar e recortar objetos na imagem yolo
+        cropped_image = detect_objects(net, image) 
+
+        if cropped_image is not None:
+            # Processar yolo
+            img = Image.fromarray(cropped_image).resize((224, 224))
+            img = np.array(img)
+            img = preprocess_input(img)
+            query_features = base_model.predict(np.expand_dims(img, axis=0)).flatten()
+            most_similar_pets = find_most_similar_pet(query_features)
+            
+            # Remover imagem temporária
+            os.remove(image_path)
+
+            return jsonify({'most_similar_pets': most_similar_pets}), 200
+        else:
+            # Remover imagem temporária
+            os.remove(image_path)
+
+            img = Image.open(file).resize((224, 224))
+            img = np.array(img)
+            img = preprocess_input(img)
+            query_features = base_model.predict(np.expand_dims(img, axis=0)).flatten()
+            most_similar_pets = find_most_similar_pet(query_features)
+            return jsonify({'most_similar_pets': most_similar_pets}), 200
+
 
 
 if __name__ == '__main__':
